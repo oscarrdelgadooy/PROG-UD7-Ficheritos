04e784106138bc85972ba9f079f3f4243798a2ae
oscarrdelgadooy <oscardelyanes@gmail.com>
2025-06-10 09:59:25 +0100

    feat(App.java): Crear método para escribir en hola.txt

--- src/App.java
+++ src/App.java
@@ -1,5 +1,37 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        saveTextInFile();
+    }
+
+    public static void saveTextInFile(){
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Dame una longitud de caracteres(30)");
+            String text = sc.nextLine();
+
+            while (text.length() < 30) {
+                System.out.println("Te faltaron " + (30 - text.length()) + " caracteres.");
+                System.out.println("Dame una longitud de caracteres(30)");
+                text = sc.nextLine();
+            }
+            text = text.toUpperCase().replace(" ", "_");
+
+            FileWriter wr = new FileWriter("./hola.txt", StandardCharsets.UTF_8);
+
+            wr.write(text);
+            wr.flush();
+
+            wr.close();
+            sc.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (NullPointerException npe) {
+            System.out.println("Ruta no encontrada");
+        }
     }
 }
